e9109a221a544f171f7f4753bfc93358c58f1faf
RiverJordan <99303113+RiverJordan@users.noreply.github.com>
2022-03-09 14:39:14 +1300

    Add files via upload

---
+++ 00_sandbox.py
@@ -0,0 +1,9 @@
+ get_number = int (input ("Choose a number?"))
+
+times_five = get_number * 5
+
+answer = ("{}Times_five is eaqual to {}" \
+          "{}".format(get_number, times_five))
+
+print (answer)
+
