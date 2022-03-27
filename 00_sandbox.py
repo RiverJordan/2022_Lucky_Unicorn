f4b171b7efe8b65bd8a52af95b727f3290be422d
River Trembath <99303113+RiverJordan@users.noreply.github.com>
2022-03-27 21:23:28 +0000

    added file

--- 00_sandbox.py
+++ 00_sandbox.py
@@ -1,9 +1,8 @@
- get_number = int (input ("Choose a number?"))
+get_number = int (input ("Choose a number?"))
 
 times_five = get_number * 5
 
-answer = ("{}Times_five is eaqual to {}" \
-          "{}".format(get_number, times_five))
+answer = ("{}Times_five is eaqual to {}".format(get_number, times_five))
 
 print (answer)
 
